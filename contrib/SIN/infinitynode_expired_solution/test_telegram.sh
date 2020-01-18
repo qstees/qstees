4816f9f04a88e6ea2e3ae669341d25f534c45a33
stevecat <stevecat@provgn.com>
2020-01-17 22:49:24 -0500

    First Commit

--- contrib/SIN/infinitynode_expired_solution/test_telegram.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. /home/$(whoami)/Telegram.sh
-if [ $tENABLED = "1" ]; then
-  curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="Test message."
-else
-  echo "Telegram is not enabled. Please set tENABLED=1 in Telegram.sh"
-fi
